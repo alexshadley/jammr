89c6f14195cc609a39202e0e2bdf4ceded3e525e
Alex Shadley <shadleyalex@gmail.com>
2020-03-24 23:39:16 -0500

    notes can be added and removed

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,10 @@
 port module Main exposing (..)
 
 import Browser
-import Process
-import Task
+import Dict
 import Html exposing (Html)
+import Task
+import Process
 
 import Element exposing (..)
 import Element.Background as Background
@@ -62,29 +63,76 @@ update msg model =
 
 ---- VIEW ----
 
+-- constants
+constLaneHeight = 25
+constRollWidth = 1000
+constLabelWidth = 50
+
+constBeatCount = 16
+constTopNote = 60
+
 
 view : Model -> Html Msg
 view model =
   let
     pitches =
-      List.range 48 60 |> List.reverse
+      List.range 36 constTopNote |> List.reverse
   in
   layout [] <| column [ centerX, padding 50, spacing 50 ]
     [ Input.button [onClick PlayTrack, centerX] {onPress = Just PlayTrack, label = text "Play Track"}
-    ,  column [width (px 1000)] (List.map noteRow pitches)
+    , column
+      ( [ width (px constRollWidth) ] ++ overlay model.track )
+      ( List.map noteRow pitches )
     ]
 
+overlay : Track -> List (Element.Attribute Msg)
+overlay track =
+  rollNotes track ++ rollDividers constBeatCount
+
+rollNotes : Track -> List (Element.Attribute Msg)
+rollNotes track =
+  track.notes
+    |> Dict.toList
+    |> List.map rollNote
+    |> List.map inFront
+
+rollNote : (Int, Note) -> Element Msg
+rollNote (id, note) =
+  el [ paddingEach
+      { top = (constTopNote - note.pitch) * constLaneHeight
+      , left = round <| note.start * (constRollWidth / constBeatCount) + constLabelWidth
+      , bottom = 0
+      , right = 0} ]
+    ( el 
+      [ width (px <| round ((4.0 / 5.0) * (constRollWidth / constBeatCount )))
+      , height (px <| round (constLaneHeight * (4.0 / 5.0)))
+      , Background.color <| rgb255 150 240 150
+      , onClick (RemoveNote id)
+      ] none
+    )
+
+
+rollDividers : Int -> List (Element.Attribute Msg)
+rollDividers n =
+  List.range 0 (n - 1)
+    |> List.map (\i -> divider (round ((toFloat i / toFloat n) * constRollWidth)))
+    |> List.map inFront
+
+divider : Int -> Element Msg
+divider x =
+  el [paddingEach {left = x, top = 0, right = 0, bottom = 0}]
+    ( el [width (px 2), height fill, Background.color <| rgb255 100 100 100] none )
 
 noteRow : Int -> Element Msg
 noteRow note =
-  row [height (px 50), width fill]
+  row [height (px constLaneHeight), width fill]
     [ noteLabel note
     , noteLane note
     ]
 
 noteLabel : Int -> Element Msg
 noteLabel note =
-  column [width (px 50)] [el [alignRight] (text (pitchToString note))]
+  column [width (px constLabelWidth)] [el [alignRight] (text (pitchToString note))]
 
 noteLane : Int -> Element Msg
 noteLane note =
@@ -94,7 +142,7 @@ noteLane note =
       False -> rgb255 230 230 230
     
     noteCells =
-      List.range 0 15
+      List.range 0 (constBeatCount - 1)
         |> List.map (noteCell note)
         |> List.intersperse noteSpacer
   in
@@ -111,6 +159,7 @@ noteCell note beat =
 noteSpacer : Element Msg
 noteSpacer =
   column [width (px 2), height fill, Background.color <| rgb255 100 100 100] []
+
 
 
 ---- PROGRAM ----
